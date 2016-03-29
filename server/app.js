bdcf9656b4bf41f26587a63d7baa1611f3ae0a65
John Ward <jsward2@gmail.com>
2016-03-28 23:39:45 -0400

    Fix up the README and license

---
+++ server/app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+
+var param;
+
+/*
+app.get('/', function (req, res) {
+	res.sendFile("/Users/john/Google Drive/active/class-gamedev/town-mmo/client/index.html",function(err){ // Transfer The File With COntent Type Text/HTML
+        if(err){
+            res.end("Sorry, Error.");
+        }else{
+            res.end(); // Send The Response
+        }
+	})
+});
+app.get('/js/game.js', function (req, res) {
+	res.sendFile("/Users/john/Google Drive/active/class-gamedev/town-mmo/client/js/game.js",function(err){ // Transfer The File With COntent Type Text/HTML
+        if(err){
+            res.end("Sorry, Error.");
+        }else{
+            res.end(); // Send The Response
+        }
+	})
+});
+*/
+
+app.get('/user', function (req, res) {
+  res.send('Got a GET request at /user');
+  console.log('Got a GET request at /user');
+});
+
+app.post('/user', function (req, res) {
+	console.log(req.route.stack);
+  res.send('Got a POST request at /user');
+  console.log('Got a POST request at /user');
+});
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+  console.log('Got a PUT request at /user');
+});
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+  console.log('Got a DELETE request at /user');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
